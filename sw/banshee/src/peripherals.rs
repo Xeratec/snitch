14cc8c0cfb28fb2b551cf5d4ed170fada2b2b376
Philip Wiese <wiesep@student.ethz.ch>
2023-02-28 11:43:31 +0100

    banshee: Formatted files & updated opcodes

--- sw/banshee/src/peripherals.rs
+++ sw/banshee/src/peripherals.rs
@@ -749,7 +749,8 @@ impl MemPoolITA {
                         // {
                         //     shift_sum += 1;
                         // }
-                        let shift_int = (current_max[j as usize] as i32) - (max[[j as usize]] as i32);
+                        let shift_int =
+                            (current_max[j as usize] as i32) - (max[[j as usize]] as i32);
                         if shift_int % 32 >= 16 {
                             shift_sum += 1;
                         }
@@ -783,9 +784,11 @@ impl MemPoolITA {
             let factor =
                 ((2.0f64.powi(8) - 1.0) * 2.0f64.powi(10)) as i32 / exp_partial_sum[j as usize];
             for k in 0..seq_len {
-                let mut shift =
-                    (((max[j as usize] as i32) - (a_requant[[0, j as usize, k as usize]] as i32)) / 32) as i32;
-                let shift_int = (max[j as usize] as i32) - (a_requant[[0, j as usize, k as usize]] as i32);
+                let mut shift = (((max[j as usize] as i32)
+                    - (a_requant[[0, j as usize, k as usize]] as i32))
+                    / 32) as i32;
+                let shift_int =
+                    (max[j as usize] as i32) - (a_requant[[0, j as usize, k as usize]] as i32);
                 if shift_int % 32 >= 16 {
                     shift += 1;
                 }
